95df0723c118df7f7190eec107ff1db9ecc3e04a
shainyachary <shainy.achary@gmail.com>
2025-05-03 01:18:02 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+let theme__container = document.querySelector(".theme__container"),
+  theme__btn = document.querySelector(".theme__btn"),
+  body = document.querySelector("body"),
+  menu = document.querySelector(".menu__bar i"),
+  navlinks = document.querySelector(".navlinks"),
+  typedText = document.querySelector("#typed"),
+  download_link = document.querySelector("#downloadBtn");
+
+// -------------menu ------------
+menu.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  if (navlinks.classList.contains("open")) {
+    menu.classList.replace("ri-menu-3-line", "ri-close-fill");
+  } else {
+    menu.classList.replace("ri-close-fill", "ri-menu-3-line");
+  }
+});
+window.addEventListener("scroll", () => {
+  navlinks.classList.remove("open");
+  menu.classList.replace("ri-close-fill", "ri-menu-3-line");
+});
+
+// ------------ typed text -----------
+const typed = new Typed(typedText, {
+  strings: ["Front-End Developer", "Full Stack Developer", "Ui Developer"],
+  typeSpeed: 40,
+  loop: true,
+});
+
+// ------------- download cv link ----------
+download_link.addEventListener("click", () => {
+  let link_cv = document.createElement("a");
+  link_cv.href = "assets/shainy-achary.pdf";
+  link_cv.download = "shain_yachary_cv.pdf";
+  link_cv.click();
+});
+// ---------- theme ----------
+theme__btn.addEventListener("click", () => {
+  body.classList.toggle("light");
+  if (body.classList.contains("light")) {
+    theme__btn
+      .querySelector("i")
+      .classList.replace("ri-moon-line", "ri-sun-line");
+  } else {
+    body.classList.remove("light");
+    theme__btn
+      .querySelector("i")
+      .classList.replace("ri-sun-line", "ri-moon-line");
+  }
+});
